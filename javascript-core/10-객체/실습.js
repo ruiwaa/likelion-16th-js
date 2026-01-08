481238f0ce0953518d151484ee173df360a879ae
ruiwaa <yeejii08@gmail.com>
2026-01-08 16:02:29 +0900

    학습: 실행컨텍스트

---
+++ javascript-core/10-객체/실습.js
@@ -0,0 +1,19 @@
+// 전역 실행 컨텍스트가 생성되면
+// 전역 객체 (Global object, windouw 객체)만들고
+//const window = new window()
+//const globalThis = window
+//this 키워드에 연결
+//const this = window
+
+
+//HTML Parser의 역할
+// .html -> text file
+// 확장자 파일을 웹브라우저가 해석해서 구조를 만들어줌
+
+//JS Engine의 역할
+//전역 실행 컨텍스트를 생성
+//전역 객체를 만들고
+//this
+//globalthis
+//window
+// -> 이렇게 세가지로 접근할 수 있다.
