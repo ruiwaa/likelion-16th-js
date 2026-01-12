5dfc728c2364b787cc065119279f94f8976253e6
ruiwaa <yeejii08@gmail.com>
2026-01-12 10:11:55 +0900

    학습: undefined 적용 사례 1. 변수 선언만 한 경우 2. 함수 반화값을 명시하지 않은 경우 3. 객체의 존재하지 않는 속성에 접근 4. 배열의 존재하지 않는 요소의 인덱스로 접근할 경우 5. 함수의 매개변수에 어떠한 값도 전달되지 않은 경우

--- javascript-core/12-Null-vs-Undefined/실습.js
+++ javascript-core/12-Null-vs-Undefined/실습.js
@@ -12,13 +12,13 @@
 // --------------------------------------------------------------------------
 
 // data 변수 선언 (값 할당하지 않음)
-
+let data // 자바스크립트 엔진이 초기화(undefined 할당,개발자 의도 없음)
 
 // data 변수 출력
-
+console.log(data)
 
 // data 변수의 타입 출력 (typeof 연산자 사용)
-
+console.log(typeof(data),String(data))
 
 // 설명:
 // 변수를 선언하고 값을 할당하지 않으면 JavaScript 엔진이 자동으로 undefined를 할당합니다.
@@ -34,13 +34,16 @@
 // --------------------------------------------------------------------------
 
 // doNothing 함수 선언 (아무것도 하지 않는 빈 함수)
-
+function doNothing(){
+// 자바스크립트가 암묵적으로 undefined 값을 할당
+// return undefined
+}
 
 // doNothing 함수 호출 결과를 result 변수에 할당
-
+const result = doNothing()
 
 // result 변수 출력
-
+console.log(result)
 
 // 설명:
 // 명시적으로 값을 반환하지 않는 함수는 기본적으로 undefined를 반환합니다.
@@ -57,15 +60,21 @@
 // name 속성: '이준'
 // age 속성: 20
 
+const user = {
+  name: '이준',
+  age: 20,
+}
 
 // user 객체의 name 속성 출력
+console.log(user.name);
 
 
 // user 객체의 email 속성 출력 (존재하지 않는 속성)
+console.log(user.email)
 
 
 // user 객체의 address 속성 출력 (존재하지 않는 속성)
-
+console.log(user.address)
 
 // 설명:
 // 객체에 존재하지 않는 속성에 접근하면 undefined가 반환됩니다.
@@ -81,16 +90,17 @@
 // --------------------------------------------------------------------------
 
 // fruits 배열 생성 ['천혜향', '애플망고']
-
+const fruits = ['천혜향', '애플망고']
 
 // fruits 배열의 0번 인덱스 출력
-
+console.log(fruits[0])
+console.log(fruits.at(0))
 
 // fruits 배열의 1번 인덱스 출력
-
+console.log(fruits[1])
 
 // fruits 배열의 2번 인덱스 출력 (존재하지 않는 인덱스)
-
+console.log(fruits[2]) 
 
 // 설명:
 // 배열에 존재하지 않는 인덱스에 접근하면 undefined가 반환됩니다.
@@ -108,10 +118,14 @@
 // greet 함수 선언
 // 매개변수: name, age
 // 기능: name과 age를 각각 출력
+function greet(name, age){
+  return 'name =' + name + ' / age =' + age 
+ 
 
+}
 
 // greet 함수 호출 ('현정'만 전달, age는 전달하지 않음)
-
+console.log(greet('현정'))
 
 // 설명:
 // 함수 매개변수에 값을 전달하지 않으면 해당 매개변수는 undefined가 됩니다.
