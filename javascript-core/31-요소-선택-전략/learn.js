666c19e8c2e3dd01f3779f718af7ef05588f6293
ruiwaa <yeejii08@gmail.com>
2026-01-23 13:14:58 +0900

    풀이: "31-요소-선택-전략" 실습

--- javascript-core/31-요소-선택-전략/learn.js
+++ javascript-core/31-요소-선택-전략/learn.js
@@ -7,24 +7,81 @@
 // 2. Class 속성이 'description'인 요소를 선택하여 콘솔에 출력하세요.
 // 3. 'button' 태그 이름을 가진 모든 요소를 선택하여 콘솔에 출력하세요.
 // 4. 'data-role' 속성값이 'confirm'인 요소를 선택하여 콘솔에 출력하세요.
-console.group('1. 상황별 선택자 활용')
+console.group("1. 상황별 선택자 활용");
 
 // 이곳에 코드를 작성하세요.
 
-console.groupEnd()
+//1번
+const mainTitle = document.querySelector("#main-title")
+console.log(mainTitle)
 
+//2번
+const descriptionClassName = document.querySelector('.description')
+console.log(descriptionClassName)
+
+//3번
+const buttonTagName = document.getElementsByTagName("button") //htmlCollection
+//const buttonTagName = document.querySelectorAll('button') //nodeList
+console.log(buttonTagName)
+
+//4번
+const is_confirmed = document.querySelector('[data-role = "confirm"]')
+console.log(is_confirmed)
+console.groupEnd()
 
 // [실습] JS 훅(js-접두사)을 활용한 상호작용 구현
 // 1. JS 훅 클래스 '.jsNotification'을 사용하여 알림창 요소를 선택하세요.
 // 2. JS 훅 클래스 '.jsHideBtn'을 사용하여 버튼 요소를 선택하세요.
 // 3. 버튼에 'click' 이벤트 리스너를 추가하여, 알림창에 'hidden' 클래스가 추가되도록 만드세요.
-console.group('2. JS 훅과 이벤트 연결')
+console.group("2. JS 훅과 이벤트 연결");
 
-const notification = null // 선택자를 작성하세요.
-const hideBtn = null      // 선택자를 작성하세요.
+//1번
+const notification = document.querySelector('.js-notification')
+console.log(notification)
+//2번
+const hideBtn = document.querySelector('.js-hide-button')  
+console.log(hideBtn)  
 
+//3번
 // 이곳에 버튼 클릭 시 알림을 숨기는 로직을 작성하세요.
+hideBtn.addEventListener('click',()=>{
+notification.classList.toggle('hidden')
+})
+console.groupEnd();
 
+//[실습] 장바구니
+//1. `id`를 사용하여"전체 선택" 버튼을 선택하세요.
+//2. `class`를 사용하여 모든 "상품 아이템(.cart-item)"을 한 번에 선택하세요.
+//3. 버튼을 클릭하면, 모든 상품 아이템에selected클래스가 추가되도록 코드를 작성하세요.
+console.group("3. 장바구니: 전체 선택자 활용")
+//1번
+const btn_select_all = document.querySelector('#btn-select-all')
+console.log(btn_select_all)
+//2번
+const cartItems = document.querySelectorAll('.cart-item')
+console.log(cartItems)
+//3번
+//forEach문
+// btn_select_all.addEventListener('click',()=>{
+//   cartItems.forEach((item)=>{
+//     item.classList.toggle('selected')
+
+//   })
+// })
+
+//for문
+// for(let i = 0, l= cartItems.length;i< l; i++){
+//   btn_select_all.addEventListener('click', ()=>{
+// cartItems[i].classList.toggle('selected')
+//   })
+ 
+// }
+
+//for of문 
+for(const item of cartItems){
+  btn_select_all.addEventListener('click', ()=>{
+    item.classList.toggle('selected')
+})}
 console.groupEnd()
 
 
